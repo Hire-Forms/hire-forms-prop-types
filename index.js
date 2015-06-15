f89f7cf17e59522b1584bcc5d278ee12e3a622c0
Gijsjan <agijsbro@gmail.com>
2015-06-15 16:19:51 +0200

    WIP

--- index.js
+++ index.js
@@ -1,49 +1,46 @@
-import React from "react";
+"use strict";
 
-export const elementOrArrayOfElement = React.PropTypes.oneOfType([
-	React.PropTypes.element,
-	React.PropTypes.arrayOf(React.PropTypes.element)
-]);
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
 
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var _react = require("react");
+
+var _react2 = _interopRequireDefault(_react);
+
+var elementOrArrayOfElement = _react2["default"].PropTypes.oneOfType([_react2["default"].PropTypes.element, _react2["default"].PropTypes.arrayOf(_react2["default"].PropTypes.element)]);
+
+exports.elementOrArrayOfElement = elementOrArrayOfElement;
 /**
- *  * A string or an object,
- *   * example: {key: "somekey", value: "somevalue"}.
- *    */
-export const stringOrKeyValue = React.PropTypes.oneOfType([
-	React.PropTypes.string,
-	React.PropTypes.shape({
-		key: React.PropTypes.string,
-		value: React.PropTypes.string
-	})
-]);
+ * A string or an object,
+ * example: {key: "somekey", value: "somevalue"}.
+ */
+var stringOrKeyValue = _react2["default"].PropTypes.oneOfType([_react2["default"].PropTypes.string, _react2["default"].PropTypes.shape({
+	key: _react2["default"].PropTypes.string,
+	value: _react2["default"].PropTypes.string
+})]);
 
-export const stringOrArray = React.PropTypes.oneOfType([
-	React.PropTypes.string,
-	React.PropTypes.array
-]);
+exports.stringOrKeyValue = stringOrKeyValue;
+var stringOrArray = _react2["default"].PropTypes.oneOfType([_react2["default"].PropTypes.string, _react2["default"].PropTypes.array]);
 
-export const stringOrArrayOfString = React.PropTypes.oneOfType([
-	React.PropTypes.string,
-	React.PropTypes.arrayOf(React.PropTypes.string)
-]);
+exports.stringOrArray = stringOrArray;
+var stringOrArrayOfString = _react2["default"].PropTypes.oneOfType([_react2["default"].PropTypes.string, _react2["default"].PropTypes.arrayOf(_react2["default"].PropTypes.string)]);
 
-export const arrayOfKeyValue = React.PropTypes.arrayOf(
-	React.PropTypes.shape({
-		key: React.PropTypes.string.isRequired,
-		value: React.PropTypes.string.isRequired
-	})
-);
+exports.stringOrArrayOfString = stringOrArrayOfString;
+var arrayOfKeyValue = _react2["default"].PropTypes.arrayOf(_react2["default"].PropTypes.shape({
+	key: _react2["default"].PropTypes.string.isRequired,
+	value: _react2["default"].PropTypes.string.isRequired
+}));
 
+exports.arrayOfKeyValue = arrayOfKeyValue;
 /**
- *  * An array of strings or an array of key/value objects,
- *   * example: [{key: "somekey", value: "somevalue"}].
- *    */
-export const arrayOfStringOrArrayOfKeyValue = React.PropTypes.oneOfType([
-	React.PropTypes.arrayOf(React.PropTypes.string),
-	React.PropTypes.arrayOf(
-		React.PropTypes.shape({
-			key: React.PropTypes.string,
-			value: React.PropTypes.string
-		})
-	)
-]);
+ * An array of strings or an array of key/value objects,
+ * example: [{key: "somekey", value: "somevalue"}].
+ */
+var arrayOfStringOrArrayOfKeyValue = _react2["default"].PropTypes.oneOfType([_react2["default"].PropTypes.arrayOf(_react2["default"].PropTypes.string), _react2["default"].PropTypes.arrayOf(_react2["default"].PropTypes.shape({
+	key: _react2["default"].PropTypes.string,
+	value: _react2["default"].PropTypes.string
+}))]);
+exports.arrayOfStringOrArrayOfKeyValue = arrayOfStringOrArrayOfKeyValue;
